4fb286ed60f2231da5c1d525e6f35e6e988bf90d
martinyde <yde001@gmail.com>
2016-05-18 09:12:28 +0200

    Added css files

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
   <link rel="stylesheet" href="css/github-markdown.css">
 
   <!-- Replace below stylesheet with your own stylesheet -->
-  <link rel="stylesheet" href="../admin/css/styles.css">
+  <link rel="stylesheet" href="./css/styles.css">
 
   <!-- prism Syntax Highlighting Styles -->
   <link rel="stylesheet" href="vendor/prism/prism.css">
