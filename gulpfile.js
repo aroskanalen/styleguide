376c3440a5f0062f08b18113600d946666bc6d64
Brian Jensen <hallo@magnify.dk>
2016-05-19 16:17:58 +0200

    #ARU-72 Refactored form, container and login

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,14 @@
 var gulp = require('gulp');
 
-
 // Plugins.
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
-var ngAnnotate = require('gulp-ng-annotate');
 var rename = require('gulp-rename');
 
-// We only want to process our own non-processed JavaScript files.
-var jsPath = ['./js/*.js', '!./js/*.min.*'];
 var sassPath = './scss/**/*.scss';
-var cssPath = './css/styles.css';
-var buildDir = './js';
-
-/**
- * Run Javascript through JSHint.
- */
-gulp.task('jshint', function() {
-  return gulp.src(jsPath)
-    .pipe(jshint())
-    .pipe(jshint.reporter(stylish));
-});
 
 /**
  * Process SCSS using libsass
@@ -33,7 +18,7 @@ gulp.task('sass', function () {
     .pipe(sass({
       outputStyle: 'nested',
       includePaths: [
-        'scss/assets/compass-mixins/lib',
+        'scss/assets/compass-mixins/lib'
       ]
     }).on('error', sass.logError))
     .pipe(gulp.dest('./css'));
@@ -43,38 +28,8 @@ gulp.task('sass', function () {
  * Watch files for changes and run tasks.
  */
 gulp.task('watch', function() {
-  gulp.watch(jsPath, ['jshint']);
   gulp.watch(sassPath, ['sass']);
 });
 
-/**
- * Watch javascript files for changes.
- */
-gulp.task('js-watch', function() {
-  gulp.watch(jsPath, ['jshint']);
-});
-
-/**
- * Build single app.js file.
- */
-gulp.task('buildJs', function () {
-  gulp.src(jsPath)
-    .pipe(uglify())
-    .pipe(rename({extname: ".min.js"}))
-    .pipe(gulp.dest(buildDir))
-});
-
-/**
- * Build single app.js file.
- */
-gulp.task('assetsJs', function () {
-  gulp.src(jsAssets)
-    .pipe(concat('assets.js'))
-    .pipe(rename({extname: ".min.js"}))
-    .pipe(gulp.dest(buildDir))
-});
-
-
-// Tasks to compile sass and watch js file.
-gulp.task('default', ['sass', 'watch']);
-gulp.task('build', ['buildJs', 'sass']);
+// Tasks to compile sass.
+gulp.task('default', ['watch']);
