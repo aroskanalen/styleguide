0c77740b1a1fe2af8e994aade67184657e0ef985
Lasse Iversen <liv@it-minds.dk>
2017-03-01 11:40:32 +0100

    Issue #OS2DISP-22: Fixed stylelint deprecatation. Removed excessive tasks from gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,171 @@
-var gulp = require('gulp');
+#!/usr/bin/env node
+
+// Plugin to handle parameters.
+var argv = require('yargs')
+.alias('t', 'theme')
+.default('theme', ['os2display'])
+  .argv;
+
+// Gulp basic.
+var gulp = require('gulp-help')(require('gulp'), {
+  'afterPrintCallback': function () {
+    var args = [];
+  }
+});
 
 // Plugins.
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
 var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
-var sassPath = './scss/**/*.scss';
+var stylelint = require('gulp-stylelint');
 
 /**
- * Process SCSS using libsass
+ * Configuration object.
  */
-gulp.task('sass', function () {
-  gulp.src(sassPath)
+var configuration = {
+  // Base theme.
+  'os2display': {
+    "sass": {
+      "paths": ['./source/**/*.scss'],
+      "srcs": ['./source/*.scss'],
+      'dest': './source/css'
+    },
+  }
+};
+
+/**
+ * Setup task for sass compilation.
+ *
+ * @param theme
+ *    Name of the theme to setup the task.
+ * @param config
+ *    Selected theme configuration object.
+ *
+ * @return string
+ *    The name of the new task.
+ */
+function sassTask(theme, config) {
+  var taskName = 'sass_' + theme;
+
+  // Process SCSS.
+  gulp.task(taskName, false, function () {
+
+    var pipe = gulp.src(config.sass.srcs)
+    .pipe(sourcemaps.init())
     .pipe(sass({
-      outputStyle: 'nested',
+      outputStyle: 'compressed',
       includePaths: [
         'scss/assets/compass-mixins/lib'
       ]
     }).on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-});
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.sass.dest));
+  });
+
+  return taskName;
+}
 
 /**
- * Watch files for changes and run tasks.
+ * Setup stylelint tasks.
+ *
+ * Note: Because stylelint task is async it returns it's gulp process.
+ *
+ * @param theme
+ *    Name of the theme to setup the task.
+ * @param config
+ *    Selected theme configuration object.
+ *
+ * @return string
+ *    The name of the new task.
  */
-gulp.task('watch', function() {
-  gulp.watch(sassPath, ['sass']);
-});
+function stylelintTask(theme, config) {
+  var taskName = 'stylelint_' + theme;
 
-// Tasks to compile sass.
-gulp.task('default', ['watch']);
+  gulp.task(taskName, false, function lintCssTask() {
+    return gulp.src(config.sass.paths)
+    .pipe(stylelint({
+      reporters: [
+        {formatter: 'string', console: true}
+      ]
+    }));
+  });
+
+  return taskName;
+}
+
+/**
+ * Watch sass and stylelint tasks.
+ *
+ * @param theme
+ *    Name of the theme to setup the task.
+ * @param config
+ *    Selected theme configuration object.
+ *
+ * @return string
+ *    The name of the new task.
+ */
+function watchTasks(theme, config) {
+  var taskName = 'watch_' + theme;
+
+  gulp.task(taskName, false, function () {
+    gulp.watch(config.sass.paths, ['sass']);
+    gulp.watch(config.sass.paths, ['stylelint']);
+
+    if (config.hasOwnProperty('js')) {
+      gulp.watch(config.js.paths, ['eslint']);
+    }
+  });
+
+  return taskName;
+}
+
+/**
+ * Dynamically setup tasks base on the selected theme.
+ *
+ * @param themes
+ *   Theme name as an string array. Used as index in the
+ *   configuration pobject.
+ */
+function setupTasks(themes) {
+  // Define task arrays.
+  var sassTaskNames = [];
+  var stylelintTaskNames = [];
+  var jsMinifyTaskNames = [];
+  var watchTasksNames = [];
+
+  // Ensure themes is an array and if not convert it.
+  if (Object.prototype.toString.call(themes) !== '[object Array]') {
+    themes = [themes];
+  }
+
+
+  // Loop over the selected themes.
+  for (var i in themes) {
+    var theme = themes[i];
+    var config = configuration[theme];
+
+    // SASS tasks.
+    sassTaskNames.push(sassTask(theme, config));
+
+    // Style-lint tasks.
+    stylelintTaskNames.push(stylelintTask(theme, config));
+
+    // Watch tasks.
+    watchTasksNames.push(watchTasks(theme, config));
+  }
+
+  // Define tasks.
+  gulp.task('sass', 'Compile SCSS into CSS', sassTaskNames);
+  gulp.task('stylelint', 'Use style-lint to check SCSS (using stylelintrc.json rules)', stylelintTaskNames);
+  gulp.task('watch', 'Watch for changes in files', watchTasksNames);
+
+  // Default task;
+  gulp.task('default', ['sass', 'stylelint']);
+}
+
+/**
+ * ------- Run task's -------
+ */
+setupTasks(argv.theme);
