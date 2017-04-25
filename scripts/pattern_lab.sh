dff97868362fbc38879fc7b4cbded860a9293f6d
Troels Ugilt Jensen <tujensen@gmail.com>
2017-04-25 08:33:28 +0200

    Issue #OS2DISP-31: Changed script to not be vagrant dependant

--- scripts/pattern_lab.sh
+++ scripts/pattern_lab.sh
@@ -1,35 +1,31 @@
 #!/usr/bin/env bash
 
-DIR=/vagrant
-
-if [[ $(whoami) != "vagrant" ]]
-then
-		echo "Please run this script from inside your vagrant box."
-		exit
-fi
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 bold=$(tput bold)
 normal=$(tput sgr0)
 
-if [ -e $DIR/htdocs/styleguide/pattern-lab ]
+if [ -e $DIR/../pattern-lab ]
 then
-		echo "${bold}Directory $DIR/htdocs/styleguide/pattern-lab already exists. Please remove it before running this script:${normal}"
+		echo "${bold}Directory $DIR/../pattern-lab already exists. Please remove it before running this script:${normal}"
 		echo
 		exit
 fi
 
-cd $DIR/htdocs/styleguide
+cd $DIR/..
 
 echo "${bold}Installing pattern lab${normal}"
 echo "${bold}When prompted for suggested starterkit choose 1: pattern-lab/starterkit-twig-base.${normal}"
 
 composer create-project pattern-lab/edition-twig-standard pattern-lab
 
-sed -i "s/publicDir:.*/publicDir: ..\/styleguide\/public/g" $DIR/htdocs/styleguide/pattern-lab/config/config.yml
-sed -i "s/sourceDir:.*/sourceDir: ..\/styleguide\/source/g" $DIR/htdocs/styleguide/pattern-lab/config/config.yml
+cp -r pattern-lab/public public
 
-cd $DIR/htdocs/styleguide/pattern-lab
+sed -i "s/publicDir:.*/publicDir: ..\/public/g" pattern-lab/config/config.yml
+sed -i "s/sourceDir:.*/sourceDir: ..\/source/g" pattern-lab/config/config.yml
+
+cd pattern-lab
 
 composer install
 
-
+php core/console --generate
